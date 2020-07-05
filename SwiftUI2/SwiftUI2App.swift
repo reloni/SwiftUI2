a4902aa93d8c421ef2c67df03454650328caa0cc
Anton Efimenko <reloni@ya.ru>
2020-07-05 21:55:45 +0300

    Initial Commit

---
+++ SwiftUI2/SwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2App.swift
+//  SwiftUI2
+//
+//  Created by Anton Efimenko on 05.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
